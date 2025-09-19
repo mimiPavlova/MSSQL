ce8957e902a239888884754dc77ecf443168e403
Мария Павлова <ivana.prego@gmail.com>
2025-09-19 19:08:28 +0300

    😺

---
+++ Exercises-DatabaseIntroduction/SQLQuery3.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Users(
+Id BIGINT PRIMARY KEY IDENTITY(1, 1),
+Username VARCHAR(30) NOT NULL,
+[Password] VARCHAR(26) NOT NULL,
+ProfilePicture VARBINARY(MAX) NULL
+CONSTRAINT _picturesize CHECK (ProfilePicture IS NULL OR DATALENGTH(ProfilePicture)<=900*1024),
+LastLoginTime DATETIME,
+IsDeleted NVARCHAR(5),
+CONSTRAINT _isDELETESTEXT CHECK (IsDeleted IN('true', 'false'))
+)
+INSERT INTO Users(Username, [Password], ProfilePicture, LastLoginTime, IsDeleted)
+VALUES
+      ('user', '12333', NULL, '2025-09-19 15:30:00', 'true'),
+       ('user', '12333', NULL, '2025-09-19 15:30:00', 'true'),
+        ('user', '12333', NULL, '2025-09-19 15:30:00', 'true'),
+         ('user', '12333', NULL, '2025-09-19 15:30:00', 'true'),
+          ('user', '12333', NULL, '2025-09-19 15:30:00', 'true');
+
+SELECT*FROM Users;
