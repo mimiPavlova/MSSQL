177e57cdbfeeba9e96469f0c0922cfa21e473ada
Мария Павлова <ivana.prego@gmail.com>
2025-10-02 20:43:13 +0300

    🐥

---
+++ Built-inFunctions/SQLQuery.sql
@@ -0,0 +1,60 @@
+
+
+
+SELECT
+A,
+H,
+(A*H)/2 AS Area FROM Triangles2
+
+SELECT PI()
+
+
+--Examle: Find the length of a line by given coordinates of the end points
+
+SELECT
+Id,
+SQRT(SQUARE(X1-X2)+SQUARE(Y1-Y2)) AS Lenght
+FROM Lines
+
+-- Calculate the required number of pallets to ship each item
+-- BoxCapacity specifies how many items can fit in one box
+-- PalletCapacity specifies how many boxes can fit in a pallet
+
+SELECT
+Id,
+CEILING(
+CEILING(
+CAST(Quantity AS FLOAT)/
+BoxCapacity
+)/
+PalletCapacity
+)
+AS [Number of pallets] 
+FROM Products
+
+
+
+--Problem: Quarterly Report
+
+--Prepare sales data for aggregation by displaying yearly quarter,
+--month, year and day of sale
+SELECT 
+InvoiceId,
+Total,
+DATEPART(QUARTER, InvoiceDate) AS Quarter,
+DATEPART(MONTH, InvoiceDate) AS Month,
+DATEPART(YEAR, InvoiceDate) AS Year,
+DATEPART(DAY, InvoiceDate) AS DAY
+FROM Invoices;
+
+--DATEDIFF
+--Example: Show employee experience
+USE SoftUni
+GO
+SELECT
+EmployeeID,FirstName, LastName, 
+DATEDIFF(YEAR, HireDate, '2025/10/02') AS [Years in service]
+FROM Employees
+
+--DATENAME--gets a sring representtion of a date's part
+SELECT DATENAME(WEEKDAY, '2025/10/02') AS WeekDay
