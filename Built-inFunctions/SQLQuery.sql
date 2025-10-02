890549f48797765ef241b3069049e35f4f170301
Мария Павлова <ivana.prego@gmail.com>
2025-10-02 21:59:48 +0300

    💛

--- Built-inFunctions/SQLQuery.sql
+++ Built-inFunctions/SQLQuery.sql
@@ -57,4 +57,19 @@ DATEDIFF(YEAR, HireDate, '2025/10/02') AS [Years in service]
 FROM Employees
 
 --DATENAME--gets a sring representtion of a date's part
-SELECT DATENAME(WEEKDAY, '2025/10/02') AS WeekDay
+SELECT DATENAME(WEEKDAY, '2005/08/26') AS WeekDay
+
+
+--converting data types
+SELECT CAST('32' AS INT)+2
+
+--offset and fetch
+
+ --**order by something and get next n columns
+
+SELECT 
+*
+FROM Employees
+ORDER BY EmployeeID
+OFFSET 1 * 10 - 10 ROWS
+FETCH NEXT 10 ROWS ONLY
